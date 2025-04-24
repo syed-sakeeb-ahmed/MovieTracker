7b79bcf4538cef87f7cae93281f049bd440cc695
Syed Ahmed <syed.sakeeb.ahmed@gmail.com>
2025-04-24 19:06:38 -0400

    Add movie page skeleton

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -5,19 +5,86 @@
     <div class="w-[1060px] h-[50px] bg-blue-400">
         Title
     </div>
-    <div class="w-[225px] h-[318px] bg-slate-300">
+    <div class="flex w-[1060px]">
+        <div class="flex flex-col">
+            <div class="w-[225px] h-[318px] bg-slate-300 shrink-0">
 
-    </div >
-    <div class="score-container">
-        <div class="score">
-            <div>
-                <div>Score</div>
-                <div>9.8</div>
-                <div>1,000,000 voters</div>
             </div>
+            <div>Known for: sfsdfsdfsd</div>
+            <div>Known for: sfsdfsdfsd</div>
+            <div>Known for: sfsdfsdfsd</div>
+            <div>Known for: sfsdfsdfsd</div>
         </div>
-        <div class="options">
 
+        <div class="score-container bg-green-500 w-full">
+            <div class="score flex bg-purple-500">
+                <div class="flex flex-col">
+                    <div>Score</div>
+                    <div>9.8</div>
+                    <div>1,000,000 voters</div>
+                </div>
+                <div class="flex ">
+                    <div>Popularity: 9.84</div>
+                    <div>Votes: 1,000,000</div>
+                </div>
+            </div>
+            <div class="options flex bg-yellow-500">
+                <div>Add to list</div>
+                <div>Rate</div>
+            </div>
+            <div class="overview bg-blue-600">
+                <div class="w-full border-b-4 border-black">Overview</div>
+                <div>The adventures of a group of explorers who make use of a newly discovered wormhole to surpass the
+                    limitations on human space travel and conquer the vast distances involved in an interstellar voyage.
+                </div>
+            </div>
+            <div class="cast bg-amber-600">
+                <div class="w-full border-b-4 border-black flex justify-between">
+                    <div>Cast</div>
+                    <div>More cast members</div>
+                </div>
+                <div class="w-[392px] h-[71px] bg-green-400 flex">
+                    <div class="w-[50px] h-[71px] bg-slate-300">
+                    </div>
+                    <div class="flex flex-col">
+                        <div>Cooper</div>
+                        <div>Matthew McConaughey</div>
+                    </div>
+                </div>
+            </div>
+            <div class="overview bg-amber-600">
+                <div class="w-full border-b-4 border-black">Recommendations</div>
+                <div class="recco-container-one-line flex">
+                    <div class="recco-component-card flex grow-0">
+                        <div class="w-[50px] h-[71px] bg-slate-300">
+
+                        </div>
+                        <div class="flex flex-col">
+                            <div>Title</div>
+                            <div>Date</div>
+                            <div>Voters</div>
+                            <div class="flex">
+                                <div>Add</div>
+                                <div>Rate</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="recco-component-card flex grow-0">
+                        <div class="w-[50px] h-[71px] bg-slate-300">
+
+                        </div>
+                        <div class="flex flex-col">
+                            <div>Title</div>
+                            <div>Date</div>
+                            <div>Voters</div>
+                            <div class="flex">
+                                <div>Add</div>
+                                <div>Rate</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
