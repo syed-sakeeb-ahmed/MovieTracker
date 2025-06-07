c239969a87541f962a8f5493a78b60623268552a
Syed Ahmed <syed.sakeeb.ahmed@gmail.com>
2025-06-06 23:45:21 -0400

    Remove height footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,7 @@
 <template>
     <div class='mt-auto w-full white'>
         <div class="flex flex-col items-center mt-[30px] mb-[30px]">
-            <div class="h-[24px]">
+            <div class="">
                 <!-- <img v-tooltip="'Resources used: TMDB\n\nLegal Notice: This product uses the TMDB API but is not endorsed or certified by TMDB.\n'"
   src="/src/assets/tmdb_logo.svg"
   alt="TMDB logo" width="24"/> -->
